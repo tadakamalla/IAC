4fb68e5cbc1b44ff95da154dce7246ce37e90c62
Aravind Tadakamalla <tadakamalla@gmail.com>
2018-04-16 11:08:14 -0400

    Update remote_tf.sh

--- remote_tf.sh
+++ remote_tf.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ssh -o StrictHostKeyChecking=no -i /home/builder/hudson/workspace/developer03309-gse00013893_devops-demo_25134.tfremote/ssh_authorized_keys.pem opc@129.213.115.185 "/home/opc/OCI-Workshop/terraform -h"
+sudo /home/opc/OCI-Workshop/remote-tf
